1fea86f7d7f454bc252f80433e1b790049ff80ae
your new username <smallmonster900@gmail.com>
2023-12-07 11:16:25 +0700

    first commit

---
+++ src/main/java/com/ra/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.ra.controller;
+
+import com.ra.model.entity.User;
+import com.ra.model.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/")
+    public String demo(Model model){
+        List<User> list = userService.findAll();
+        model.addAttribute("list",list);
+        return "home";
+    }
+}
